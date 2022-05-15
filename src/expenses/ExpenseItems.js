ea57b7a0590c362f60f114e29582fc46260db6cb
khushijain3103 <khushijain312003@gmail.com>
2022-05-15 14:48:31 +0530

    learning react

---
+++ src/expenses/ExpenseItems.js
@@ -0,0 +1,14 @@
+import './ExpenseItems.css';
+
+function ExpenseItems (){
+    return(
+        <div className="expense-items">
+            <div className="expense_date">expense date</div>
+            <div className="expense-title">expense title</div>
+            <div className="expense-rupees">expense rupees</div>
+        </div>
+        
+    );
+}
+
+export default ExpenseItems
